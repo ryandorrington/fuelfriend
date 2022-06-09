823b235efe5b6f5f1f996a19f7f022920d9587c8
Ryan Dorrington <dorringtonryan@gmail.com>
2022-06-09 22:18:01 +1000

    start implementation of frontend

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require("express");
 var axios = require("axios");
 var fs = require("fs");
+const ejsMate = require("ejs-mate");
+const path = require("path");
 const getNearbyStations = require("./getNearbyStations");
 const getViableRoutes = require("./getViableRoutes");
 const getFuelPricesOfViableStations = require("./getFuelPricesOfViableStations");
 const getRecommendedStation = require("./getRecommendedStation");
 
 const app = express();
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 const port = 3000;
 
 const origin = "72+wimbledon+drive+kingsley+WA";
-const destination = "26+walters+drive+osbornepark+WA";
+const destination = "99+shepperton+road+victoria+park";
 
 const getFuelPriceData = () => {
 	return JSON.parse(fs.readFileSync("stations.json"));
@@ -28,14 +33,22 @@ const directionsResponseExample = async (origin, destination) => {
 	return response;
 };
 
-app.get("/", async (req, res) => {
+const createRouteLink = (placeID) => {
+	return `https://www.google.com/maps/dir/?api=1&origin=${origin}&destination=${destination}&waypoints=FuelStation&waypoint_place_ids=${placeID}`;
+};
+
+const getFuelRoute = async () => {
 	const routeWithoutFuelStop = await directionsResponseExample(origin, destination);
 	const stations = await getNearbyStations(routeWithoutFuelStop);
 	const viableRoutes = await getViableRoutes(routeWithoutFuelStop, stations, origin, destination, 6 * 60);
 	getFuelPricesOfViableStations(getFuelPriceData(), viableRoutes);
 	const recommendedStation = getRecommendedStation(viableRoutes);
-	console.log("the best route is: " + recommendedStation.userID);
-	res.send("hello");
+	console.log("the best route is: " + createRouteLink(recommendedStation.station));
+};
+
+app.get("/", (req, res) => {
+	// getFuelRoute()
+	res.render("app");
 });
 
 app.listen(port, () => {
