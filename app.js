b2560d25919e9483918f853d199b86242e2ef6ea
Ryan Dorrington <dorringtonryan@gmail.com>
2022-06-14 14:59:35 +1000

    add start script

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const getFuelPriceData = () => {
 	return JSON.parse(fs.readFileSync("stations.json"));
