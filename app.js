9d72646f6e65a0bf03db918575fc87b94fa17391
Ryan Dorrington <dorringtonryan@gmail.com>
2022-06-14 19:52:25 +1000

    style

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 const port = process.env.PORT || 3000;
 
 const getFuelPriceData = () => {
